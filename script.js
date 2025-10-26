e00cd86a8bbc80d68d3fd96dc2803136b6ecc825
Ayanokoij <111720@st-maartenscollege.nl>
2025-10-26 20:41:31 +0000

    piramide voor aan de achterkant en begin extra object

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Aantal levens Speler
 var JosLevens = 3;
-var fillRed = false
+
+// variabelen voor piramide
+var aantalLagen = 12;
+var breedte;
+var hoogte;
 
 // Het maken van het Raster
 class Raster {
@@ -20,43 +24,34 @@ class Raster {
     stroke('grey');
 
     // Ervoor zorgen dat er een blauwe rand is en als je het raakt dat het rood wordt. 
-    
     for (var rij = 0;rij < this.aantalRijen;rij++) {
-      for (var kolom = 0;kolom < this.aantalKolommen;kolom++) {
-        if (rij == 0 || rij == 11 || kolom == 0 || kolom == 17) {
-          if (fillRed) {
-            fill('red');
-            rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
-          }
-          else {
-          fill('blue');
-          rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
-          }
-          
-          if (dist(kolom*this.celGrootte,rij*this.celGrootte, mouseX, mouseY) < this.celGrootte) {
-             fillRed = true
+          for (var kolom = 0;kolom < this.aantalKolommen;kolom++) {
+            
+            if (rij == 0 || rij == 11 || kolom == 0 || kolom == 17) {
+
+              // als de cel een deel van de rand is wordt deze blauw.
               
+              fill('blue');
+              rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+
+              // als de muis over de cel gaat wordt deze rood.
+              if (dist(kolom*this.celGrootte,rij*this.celGrootte, mouseX, mouseY) < this.celGrootte) {
+                
+                fill('red')
+                rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+              }
+              // als een cel niet deel uitmaakt van de rand krijgt deze geen vulling/kleur.
             }
-          else {
-            fillRed = false
-          }
-          }
-        else {
-          if (fillRed==true) {
-            fill('red');
+            else {
+              noFill();
             rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
-          }
-          else {
-          nofill();
-          rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+            }
           }
         }
-
+        pop();
       }
     }
-    pop();
-  }
-}
+
 
 // EINDE raster
 
@@ -99,7 +94,7 @@ class Jos {
   }
 
 
-// Als hij wordt geraakt door een ander beweegend object
+// Als hij wordt geraakt door een ander bewegend object
   wordtGeraakt(vijand) {
     if (this.x == vijand.x && this.y == vijand.y) {
       return true;
@@ -108,6 +103,7 @@ class Jos {
       return false;
     }
   }
+// checkt of jos een raket raakt
   wordtGeraakt(raket) {
     if (dist(raket.x, raket.y, this.x, this.y) < 50) {
       return true;
@@ -117,7 +113,7 @@ class Jos {
     }
 
   }
-
+// Checkt of jost de pingpongbal raakt
   wordtGeraakt(pingpong) {
     if (dist(pingpong.x, pingpong.y, this.x, this.y) < 40) {
       return true;
@@ -147,12 +143,18 @@ class raket {
     this.y += this.vy;
     if (this.y > canvas.height - this.diameter / 2 || this.y < this.diameter / 2) {
       this.vy *= -1;
+      // de raket klapt om als hij de rand raakt
+
+      //De raket wordt een klein stukje verplaatst als hij de rand raakt, zodat hij niet in de rand blijft hangen.
+      this.y -= 1;
     }
     if (this.y < 25){
       this.vy *= 1.25;
+      // de raket versnelt telkens met 1.25 als deze naar beneden gaat
     }
     if(this.y > canvas.height - this.diameter / 2){
         this.vy /= 1.25;
+      // de raket remt telkens af met 1.25 als deze naar boven gaat
     }
   }
   teken() {
@@ -178,7 +180,7 @@ class pingpong {
   beweeg() {
     this.x += this.snelheidX;
     this.y += this.snelheidY;
-
+// de snelheid van de bal klapt om als hij de rand raakt. Dit geld voor bijde x en y waardes.
     if (this.x < this.straal || this.x > canvas.width - this.straal) {
       this.snelheidX *= -1;
     }
@@ -186,7 +188,7 @@ class pingpong {
       this.snelheidY *= -1;
     }
   }
-
+// tekenen pingpongbal
   teken() {
     fill('white');
     ellipse(this.x,this.y,this.diameter);
@@ -225,24 +227,29 @@ function preload() {
 }
 
 function setup() {
+
   canvas = createCanvas(900,600);
   canvas.parent();
   frameRate(10);
   textFont("Verdana");
   textSize(20);
-
+// bepaalt hoeveel rijen en kolommen er zijn
   raster = new Raster(12,18);
-
+// functie aanroepen
   raster.berekenCelGrootte();
 
-// objecten aanmaken
+  // bepalen hoe groot de piramide is per vakje
+  breedte = 50;
+  hoogte = 25;
+  
+// Speler aanmaken
   eve = new Jos();
   eve.stapGrootte = 1*raster.celGrootte;
   for (var b = 0;b < 6;b++) {
     frameEve = loadImage("images/sprites/Eve100px/Eve_" + b + ".png");
     eve.animatie.push(frameEve);
   }
-
+// vijanden aanmaken
   alice = new Vijand(700,200);
   alice.stapGrootte = 1*eve.stapGrootte;
   alice.sprite = loadImage("images/sprites/Alice100px/Alice.png");
@@ -265,9 +272,31 @@ function setup() {
 // EINDE  objcten aanmaken
 
 // Speler raakt een object + einde/doorloop spel/finish game.
+
+// tekenen van de omgekeerdepiramide
+/*
+function tekenPiramide(n) {
+  push();
+  fill('red');
+  if (n>0) {
+    for (var nr = 1;nr < n;nr++) {
+      rect(nr*breedte,0,breedte,hoogte);
+    }
+    
+     translate(breedte/2,hoogte);
+    n--;
+    tekenPiramide(n);
+    pop();
+    
+  }
+}
+*/
 function WinOfVerlies() {
+
+  // checkt of deze gebeurtenissen plaatsvinden
   if (eve.wordtGeraakt(alice) || eve.wordtGeraakt(bob) || eve.wordtGeraakt(cindy) || eve.wordtGeraakt(raket1) || eve.wordtGeraakt(raket2) || eve.wordtGeraakt(raket3))
     {
+      //  als Jos nog levens heeft, dan gaat hij terug naar de startpositie en verliest 1 leven
       if (JosLevens > 1) {  
         JosLevens --;
         eve.x = 0;
@@ -279,6 +308,7 @@ function WinOfVerlies() {
         cindy.x = 900;
         cindy.y = 50;
       }
+        // als Jos geen levens meer heeft, dan is het spel afgelopen
       else {
         background('red')
         fill('white');
@@ -287,6 +317,7 @@ function WinOfVerlies() {
         noLoop();
       }
     }
+  // als Jos de finish bereikt, dan heb je gewonnen
   if (eve.gehaald) {
     background('green');
     fill('white');
@@ -295,6 +326,7 @@ function WinOfVerlies() {
     noLoop();
   }
 }
+  // Deze functie geeft je een extra elven als de pingpongbal aanraakt. Ook verdwijnt de pingpongbal van het speelveld.
   function ExtraLeven() {
     if ( eve.wordtGeraakt(pingpong1))
       {
@@ -308,8 +340,10 @@ function WinOfVerlies() {
 // alles tekenen
 function draw() {
   background(brug);
+  /*
+  tekenPiramide(aantalLagen);
+  */
   raster.teken();
-
   eve.beweeg();
   alice.beweeg();
   bob.beweeg();
@@ -328,8 +362,12 @@ function draw() {
 
   pingpong1.beweeg();
   pingpong1.teken();
+
+  // laat levens zien
   
   text("Levens: " + JosLevens, 10, 20)
+
+  // functies aanroepen
   WinOfVerlies();
   ExtraLeven();
 
